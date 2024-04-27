99ebdbc36e5915fcda7f8aee7790946befd3dabb
anik_8teen <20-42695-1@student.aiub.edu>
2024-04-27 11:47:02 +0600

    first commit

---
+++ app/Models/projects.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class projects extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'title',
+        'description',
+        'start_date',
+        'end_date',
+    ];
+}
